eadf5c42b61370460875dbf0318620610773e719
jonnii <me@jonnii.com>
2021-08-13 17:18:49 -0400

    generate some swagger

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,26 @@
 package com.example
 
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.media.Schema
+
 fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("com.example")
-		.start()
+    build()
+        .args(*args)
+        .packages("com.example")
+        .start()
 }
 
+@Controller("/v1/things")
+class ThingsController {
+    @Get
+    suspend fun getThing(): Thing {
+        return Thing("super thing")
+    }
+}
+
+@Schema(name = "Thing")
+data class Thing(
+    val thingName: String
+)
